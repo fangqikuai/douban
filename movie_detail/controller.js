2e2fe8882082e24f5e14655467a7bf6698e5c0d0
fang qikuai <695051791@qq.com>
2016-11-10 21:46:44 +0800

    doubanmovies

---
+++ movie_detail/controller.js
@@ -0,0 +1,44 @@
+(function(angular) {
+  'use strict';
+
+  // 创建正在热映模块
+  var module = angular.module(
+    'moviecat.movie_detail', [
+      'ngRoute',
+      'moviecat.services.http'
+    ]);
+  // 配置模块的路由
+  module.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/detail/:id', {
+      templateUrl: 'movie_detail/view.html',
+      controller: 'MovieDetailController'
+    });
+  }]);
+
+  module.controller('MovieDetailController', [
+    '$scope',
+    '$route',
+    '$routeParams',
+    'HttpService',
+    'AppConfig',
+    '$window',
+    function($scope, $route, $routeParams, HttpService, AppConfig,$window) {
+      $scope.movie = {};
+      $scope.loading = true;
+      var id = $routeParams.id;
+
+      var apiAddress = AppConfig.detailApiAddress + id;
+
+      // 跨域的方式
+      HttpService.jsonp(apiAddress, {}, function(data) {
+        $scope.movie = data;
+        console.log($scope.movie);
+        $scope.loading = false;
+        $scope.$apply();
+      });
+      $scope.back = function(){
+        $window.history.back();
+      }
+    }
+  ]);
+})(angular);
